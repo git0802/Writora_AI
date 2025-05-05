9f4a1e36a205165b03ef48e80c4ca939a5bb04f4
LoveBee <lovebee0802@gmail.com>
2025-05-05 03:10:51 -0400

    Revert "feat: add mobile navigation and main navbar components"
    This reverts commit b433d8a9beb9e69fab623f2b3b398f35b70acf28.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import { cn } from "@/lib/utils";
 import { aeonik, inter } from "@/lib/constants/fonts";
 import { ORIGIN_URL } from "@/lib/constants/pricing";
-import Navbar from "@/components/home/navbar";
-import Footer from "@/components/home/footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Writora AI",
@@ -54,10 +63,8 @@ export default function RootLayout({
           )}
         >
           <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] bg-neutral-950" />
-          <Navbar />
           <main className="relative z-0 mx-auto mt-20 w-full">{children}</main>
           <Toaster richColors position="top-right" />
-          <Footer />
         </body>
       </html>
     </ClerkProvider>
